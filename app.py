6b54b9059cff157c61e9d3181fa5e73751aae6a0
Manos <manoskakarakis@gmail.com>
2024-09-18 22:50:30 +0300

    bugssss

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from datetime import datetime
+import requests
 from flask import Flask, jsonify
-from config import Config
-from utils import get_iss_location, get_iss_pass_times
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+class Config:
+    DEBUG = True
+    HOST = '0.0.0.0'
+    PORT = '5000'
+    ISS_API_BASE_URL = 'http://api.open-notify.org'
 
 
 @app.route('/api/iss-location', methods=['GET'])
@@ -13,9 +18,8 @@ def iss_location():
     return jsonify(location)
 
 
-@app.route('/api/iss-pass/<lat>/<lon>', methods=['GET'])
+@app.route('/api/iss-pass/<floa:lat>/<float:lon>', methods=['GET'])
 def iss_pass(lat, lon):
-    print(lat, lon)
     pass_times = get_iss_pass_times(lat, lon)
     return jsonify(pass_times)
 
@@ -23,3 +27,38 @@ def iss_pass(lat, lon):
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'],
             host=app.config['HOST'], port=app.config['PORT'])
+
+
+def get_iss_location():
+    response = requests.get(f"{Config.ISS_API_BASE_URL}/iss-now.json")
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            'timestamp': data['timestamp'],
+            'latitude': data['iss_position']['latitude'],
+            'longitude': data['iss_position']['longitude']
+        }
+    else:
+        return {'error': 'Unable to fetch ISS location'}
+
+
+def get_iss_pass_times(lat, lon):
+    params = {
+        'lat': lat,
+        'lon': lon
+    }
+    response = requests.get(
+        f"{Config.ISS_API_BASE_URL}/iss-pass.json", params=params)
+    if response.status_code = 200:
+        data = response.json()
+        return {
+            'latitude': data['request']['latitude'],
+            'longitude': data['request']['longitude'],
+            'passes': data['response']
+        }
+    else
+    return {'error': 'Unable to fetch ISS pass times'}
+
+
+def convert_timestamp(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    DEBUG = True
-    HOST = '0.0.0.0'
-    PORT = 5003
-    ISS_API_BASE_URL = 'http://api.open-notify.org'
